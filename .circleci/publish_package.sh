c82d258689eb652221fa1f4f81fe0161e8721fd9
Rammsoad-Sixx <101249260+Rammsoad-Sixx@users.noreply.github.com>
2022-09-09 14:03:37 -0700

    Update publish_package.sh

--- .circleci/publish_package.sh
+++ .circleci/publish_package.sh
@@ -3,7 +3,7 @@ set -ex
 
 # Determine the version from the tag; remove the leading v "v3.3.0" -> "3.3.0"
 # HOST="https://wework.jfrog.io/artifactory/api/composer/php-local"
-HOST="https://wework.jfrog.io/artifactory/php-local"
+HOST="https://rafapaz.jfrog.io/artifactory/composer-php-local"
 VERSION=$(echo "${CIRCLE_TAG}" | sed 's/^v//')
 VERSION=1.0.0
 echo "${VERSION}"
@@ -11,4 +11,4 @@ FILE_NAME="welkio-error-$VERSION.zip"
 git archive -o "$FILE_NAME" HEAD
 
 # curl al rest api
-curl -u "$ARTIFACTORY_USER:$ARTIFACTORY_APIKEY" -X PUT "$HOST/wework/welkio-error/$FILE_NAME" -T "$FILE_NAME"
+curl -u "$ARTIFACTORY_USER:$ARTIFACTORY_APIKEY" -X PUT "$HOST/circleci/welkio-error/$FILE_NAME" -T "$FILE_NAME"
