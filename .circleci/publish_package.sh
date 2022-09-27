d88a0d26597dda82255d6ce0b2846dec8183dbca
rpaz-wl <roberto.paz@wizeline.com>
2022-09-27 13:41:10 -0500

    Testing circle ci

--- .circleci/publish_package.sh
+++ .circleci/publish_package.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -ex
 
-# Determine the version from the tag; remove the leading v "v3.3.0" -> "3.3.0"
 # HOST="https://wework.jfrog.io/artifactory/api/composer/php-local"
 HOST="https://rafapaz.jfrog.io/artifactory/composer-php-local"
-VERSION=$(echo "${CIRCLE_TAG}" | sed 's/^v//')
-VERSION=1.0.0
+# Determine the version from the tag; remove the leading v "v3.3.0" -> "3.3.0"
+LATEST_TAG=$(git describe --tags --abbrev=0)
+VERSION=$(echo "${LATEST_TAG}" | sed 's/^v//')
 echo "${VERSION}"
-FILE_NAME="welkio-error-$VERSION.zip"
+FILE_NAME="rafapaz-test-$VERSION.zip"
 git archive -o "$FILE_NAME" HEAD
 
 # curl al rest api
-curl -u "$ARTIFACTORY_USER:$ARTIFACTORY_APIKEY" -X PUT "$HOST/circleci/welkio-error/$FILE_NAME" -T "$FILE_NAME"
+# curl -u "$ARTIFACTORY_USER:$ARTIFACTORY_APIKEY" -X PUT "$HOST/circleci/welkio-error/$FILE_NAME" -T "$FILE_NAME"
